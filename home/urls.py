0772744d1562851fd8c641fc41c7c7a267de0783
benab04 <benabrahambiju@gmail.com>
2025-04-17 10:16:09 +0530

    Clean code files

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('test_blocker/', views.test_endpoint, name='blocker'),
     path('test_blocker_temporary/', views.test_endpoint_2, name='temp_blocker'),
-    path('time/', views.get_time, name='get_time'),
+    # path('time/', views.get_time, name='get_time'),
 ]
